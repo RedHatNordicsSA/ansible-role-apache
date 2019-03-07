55923819fb2b18acff5c5fc2c71fa5187f06b49c
Peter Gustafsson <pgustafs@redhat.com>
2019-03-07 13:36:40 +0100

    added Testinfra tests

--- molecule/shared/tests/test_default.py
+++ molecule/shared/tests/test_default.py
@@ -6,14 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_passwd_file(host):
-    passwd = host.file("/etc/passwd")
-    assert passwd.contains("root")
-    assert passwd.user == "root"
-    assert passwd.group == "root"
-    assert passwd.mode == 0o644
-
-
 def test_apache_is_installed(host):
     apache = host.package("httpd")
     assert apache.is_installed
@@ -23,3 +15,8 @@ def test_apache_running_and_enabled(host):
     apache = host.service("httpd")
     assert apache.is_running
     assert apache.is_enabled
+
+
+def test_port_80_is_listening(host):
+    socket = host.socket("tcp://80")
+    assert(socket.is_listening)
